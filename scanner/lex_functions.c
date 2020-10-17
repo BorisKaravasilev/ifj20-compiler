e1284d5a1ad7cb272492ebdbd5de0abb67f58d88
VeciCZ <xvecer23@stud.fit.vutbr.cz>
2020-10-17 17:13:54 +0200

    Added support for ranges of accepted characters

--- scanner/lex_functions.c
+++ scanner/lex_functions.c
@@ -1,9 +1,5 @@
-#include <stdbool.h>
-#include <stdio.h>
-
 #include "lex_functions.h"
 #include "token_types.h"
-#include "token_functions.h"
 
 #define ERROR_NO_NEXT_STATE -2
 
@@ -13,6 +9,29 @@
 // Symbols that should be ignored (eg., blank spaces, line endings ...)
 char skip_symbols[] = {' ', '\n'};
 int skip_symbols_len = sizeof(skip_symbols) / sizeof(char);
+
+bool is_accepted(char sym, range_or_charT rule_symbols) {
+    if (rule_symbols.single_char != -1){ // rule accepts a single character
+        if (rule_symbols.single_char == sym) // is the current symbol same as the accepted character?
+            return true;
+        else
+            return false;
+    }
+    else { // rule accepts one or multiple ranges or multiple individual characters
+        for (int i = 0; i < RANGES_IN_RULE; i++) { // go through all available range slots
+            if (rule_symbols.range[i].to == -1) { // individual character
+                if (rule_symbols.range[i].from == sym)
+                    return true;
+            }
+            else { // range
+                if (rule_symbols.range[i].from <= sym && sym <= rule_symbols.range[i].to)
+                    return true;
+            }
+        }
+
+        return false;
+    }
+}
 
 int get_next_state(char curr_sym, int curr_state, finite_automataT *fa) {
     // Go through all rules in FA
@@ -24,8 +43,7 @@ int get_next_state(char curr_sym, int curr_state, finite_automataT *fa) {
                 // Go through all transition symbols in rule
                 // (symbols that bring you to next state)
                 for (int j = 0; j < TRANS_SYM_LEN; j++) {
-                    // TODO: Change to is_in_range()
-                    if (fa->rules[i].transition_symbols[j] == curr_sym) {
+                    if (is_accepted(curr_sym, fa->rules[i].transition_symbols)) {
                         int next_state = fa->rules[i].to_state;
                         curr_state = next_state;
                         return next_state;
@@ -38,14 +56,6 @@ int get_next_state(char curr_sym, int curr_state, finite_automataT *fa) {
     // Can't transition to any state with curr_sym
     return ERROR_NO_NEXT_STATE;
 }
-
-//bool is_in_range() {
-    // is 'ragne_or_char.from == -1'
-    // -> then compare singe_char to current char
-    // else
-    // if (from <= curr && curr <= to)
-    // -> return true
-//}
 
 bool is_final_state(int state, finite_automataT *fa) {
     for (int i = 0; i < FINAL_STATES_LEN; i++) {
--- scanner/lex_functions.h
+++ scanner/lex_functions.h
@@ -2,9 +2,11 @@
 #define __LEX_FUNCTIONS_H_
 
 #include <stdio.h>
+#include <stdbool.h>
 #include "lex_structures.h"
 #include "token_functions.h"
 
 token_struct get_next_token(finite_automataT *fa, FILE *input_file, token_struct *token);
+bool is_accepted(char sym, range_or_charT rule_symbols);
 
 #endif
