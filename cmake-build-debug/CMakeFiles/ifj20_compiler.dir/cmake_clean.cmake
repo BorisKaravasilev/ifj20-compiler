e2c8fc4fff3f076272164441f13493fc93774d28
Petr Vrtal <xvrtal01@vutbr.cz>
2020-10-31 16:20:01 +0100

    Changed project to Cmake project
    Project debugging now works using clion cmake configuration.

---
+++ cmake-build-debug/CMakeFiles/ifj20_compiler.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ifj20_compiler.dir/general/string_functions.c.o"
+  "CMakeFiles/ifj20_compiler.dir/general/utility_functions.c.o"
+  "CMakeFiles/ifj20_compiler.dir/parser/expression_analysis.c.o"
+  "CMakeFiles/ifj20_compiler.dir/scanner/finite_automata.c.o"
+  "CMakeFiles/ifj20_compiler.dir/scanner/scanner_functions.c.o"
+  "CMakeFiles/ifj20_compiler.dir/scanner/stack.c.o"
+  "CMakeFiles/ifj20_compiler.dir/scanner/symtable.c.o"
+  "CMakeFiles/ifj20_compiler.dir/scanner/token_functions.c.o"
+  "CMakeFiles/ifj20_compiler.dir/ifj20-compiler.c.o"
+  "ifj20_compiler.pdb"
+  "ifj20_compiler"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ifj20_compiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
