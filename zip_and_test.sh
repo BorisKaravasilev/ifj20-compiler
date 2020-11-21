df2194474371800a6c749a7a8a7f06604514f00b
Boris Karavasilev <xkarav01@vutbr.cz>
2020-11-21 16:00:14 +0100

    Fixed scripts for testing on Merlin

--- zip_and_test.sh
+++ zip_and_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SRC_FOLDER="./zdrojaky"
+TMP_SRC_FOLDER="./zdrojaky"
 echo
 
 # flattening aktualniho adresare
@@ -9,7 +9,7 @@ function flattening () {
   local FILES=`find ${F_FOLDER} -name '*' -type f`
   local err=""
   for FILE in $FILES; do
-    NFILE=${SRC_FOLDER}/${FILE##*/}  # get the filename behind the last / in the path (or use basename) and add prefix "./"          
+    NFILE=${TMP_SRC_FOLDER}/${FILE##*/}  # get the filename behind the last / in the path (or use basename) and add prefix "./"          
     if [ "$FILE" != "$NFILE" ]; then
       cp "$FILE" ${NFILE} 2>/dev/null
       echo "Copied: ($FILE -> $NFILE)"
@@ -19,11 +19,11 @@ function flattening () {
 
 function delete_dirs () {
     rm -r testdir > /dev/null 2>&1 # Delete testdir
-    rm -r $SRC_FOLDER > /dev/null 2>&1 # Remove src folder
+    rm -r $TMP_SRC_FOLDER > /dev/null 2>&1 # Remove src folder
 }
 
 delete_dirs
-mkdir $SRC_FOLDER
+mkdir $TMP_SRC_FOLDER
 
 F_FOLDER="./general"
 flattening
@@ -32,12 +32,12 @@ flattening
 F_FOLDER="./scanner"
 flattening
 
-cp ifj20-compiler.c ${SRC_FOLDER}/ifj20-compiler.c
+cp ifj20-compiler.c ${TMP_SRC_FOLDER}/ifj20-compiler.c
 
 # Zips all files, runs is_it_ok.sh
 
 ZIP="xkarav01.zip"
-TO_ZIP="${SRC_FOLDER}/* Makefile rozdeleni dokumentace.pdf"
+TO_ZIP="${TMP_SRC_FOLDER}/* Makefile rozdeleni dokumentace.pdf"
 
 rm -r $ZIP  > /dev/null 2>&1 # Delete old zip
 
@@ -52,6 +52,6 @@ echo "Running is_it_ok..."
 ./is_it_ok.sh $ZIP testdir
 
 echo
-echo "Deleting testdir, ${SRC_FOLDER}"
+echo "Deleting testdir, ${TMP_SRC_FOLDER}"
 
 delete_dirs
